9242c880511fd1ffa16be9dc5cafb478811f6718
Bubzieiscool <kovanninja@gmail.com>
2024-10-22 15:30:00 +0300

    Bubzies Hub.lua

---
+++ gui.lua
@@ -0,0 +1,20 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Bubzie's Hub", "DarkTheme")
+
+ -- MAIN
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Script hub")
+
+
+MainSection:NewButton("Pendelum Hub", "Test", function()
+    local response = game:HttpGet("https://paste.ee/d/JR0bn")
+loadstring(response)()
+end)
+
+MainSection:NewButton("Pendelum Hub", "Test", function()
+    MainSection:NewButton("Redz Hub Blox fruits.", "Test", function()
+    local response = game:HttpGet("https://paste.ee/d/JR0bn")
+loadstring(response)()
+end)
+loadstring(response)()
+end)
